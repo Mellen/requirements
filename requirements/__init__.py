173e18eb6ffc83367edd3f52f69cbaa7f9efbeb1
mattack108 <matt.lenc@gmail.com>
2013-06-14 17:02:56 +0100

    Add sentry app

--- requirements/__init__.py
+++ requirements/__init__.py
@@ -1,6 +1,11 @@
+import os
+
 from flask import Flask
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
+sentry = Sentry(app, dsn=os.environ.get('SENTRY_DSN'))
+sentry.init_app(app)
 # TODO: fix it to work on 'true' wsgi server
 # app.config.from_object('requirements.settings')
 
