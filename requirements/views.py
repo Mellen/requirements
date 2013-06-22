e96d596b223df1e12638beacd393d9f73a5e79e9
mattack108 <matt.lenc@gmail.com>
2013-06-22 18:21:09 +0100

    Return raw content

--- requirements/views.py
+++ requirements/views.py
@@ -48,7 +48,7 @@ def create_user(token):
 def sync():
     if 'github_token' in session:
         s = github.get('user/repos')
-        return str(s)
+        return s.raw_data
     return redirect(url_for('login'))
 
 
