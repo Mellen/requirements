41d7b79c27ed932955d503fe3c7c463b85f51af2
mattack108 <matt.lenc@gmail.com>
2013-06-22 15:20:14 +0100

    Flask config object is different, wat

--- requirements/github.py
+++ requirements/github.py
@@ -4,11 +4,10 @@ from requirements import app
 
 oauth = OAuth(app)
 
-
 github = oauth.remote_app(
     'github',
-    consumer_key=app.config.get('GH_CLIENT_ID'),
-    consumer_secret=app.config.get('GH_CLIENT_CLIENT'),
+    consumer_key=app.config['GH_CLIENT_ID'],
+    consumer_secret=app.config['GH_CLIENT_CLIENT'],
     request_token_params={'scope': 'repo'},
     base_url='https://api.github.com/',
     request_token_url=None,
