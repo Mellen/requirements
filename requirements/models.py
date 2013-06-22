465b40c5e8b1ef29f4abfd3d17923289bb468b45
Matt Ellen <matthew.ellen@googlemail.com>
2013-06-22 12:36:59 +0100

    created many to many relationship for organisations to members on the user table

--- requirements/models.py
+++ requirements/models.py
@@ -8,8 +8,7 @@ db.init_app(app)
 
 member_org_map = db.Table('member_org_map',
                           db.Column('member_id', db.Integer, db.ForeignKey('user.id')),
-                          db.Column('org_id', db.Integer, db.ForeignKey('user.id'))
-)
+                          db.Column('org_id', db.Integer, db.ForeignKey('user.id')))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,11 +16,15 @@ class User(db.Model):
     github_access_token = db.Column(db.Integer)
     is_member = db.Column(db.Boolean)
     repos = []
+    orgs = db.relationship('User',
+                           secondary=member_org_map,
+                           backref=db.backref('members', lazy='dynamic'))
 
     def __init__(self, username, github_access_token, is_member):
         self.username = username
         self.github_access_token = github_access_token
         self.is_member = is_member
+        
 
     def __repr__(self):
         return '<User %s>' % self.username
