0fd6fea01977cff415cbec81de58d9d0256a8736
Matt Ellen <matthew.ellen@googlemail.com>
2013-06-22 14:50:31 +0100

    adding suggested changes removed methods that don't do anything

--- requirements/models.py
+++ requirements/models.py
@@ -27,33 +27,21 @@ class User(db.Model):
     def __repr__(self):
         return '<User %s>' % self.username
 
-    def update_repo_list(self):
-        pass
-
 class Repo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     repo_name = db.Column(db.String)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
-    def __init__(self):
-        pass
-
-class PyPiLibrary(db.Model):
-    lib_name = db.Column(db.String, primary_key=True)
+class Package(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    package = db.Column(db.String, unique=True)
     version = db.Column(db.String)
-    last_fetch_date_time = db.Column(db.DateTime)
-
-    def __init__(self):
-        pass
+    last_fetch_date_time = db.Column(db.DateTime, default=db.func.now, onupdate=db.func.now)
 
 class Sync(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     repo_id = db.Column(db.Integer, db.ForeignKey('repo.id'))
     lib_name = db.Column(db.Integer, db.ForeignKey('pypilibrary.lib_name'))
-    last_sync_date_time = db.Column(db.DateTime)
+    last_sync_date_time = db.Column(db.DateTime, default=db.func.now, onupdate=db.func.now)
     last_sync_user = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
-    def __init__(self):
-        pass
-
     
