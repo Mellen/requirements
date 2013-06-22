e5ac50df93b5f1a248c2146c27ee210a2c3af3d0
mattack108 <matt.lenc@gmail.com>
2013-06-22 20:01:11 +0100

    Get user repos and org's repos the user is member of

--- requirements/views.py
+++ requirements/views.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 from flask import (
@@ -7,19 +8,6 @@ from flask import (
 from requirements import app
 from requirements.github import github
 from requirements.models import db, User
-
-
-def p(s):
-    print s
-    sys.stdout.flush()
-
-
-# @app.before_request
-# def before_request():
-#     token = 'f49a6d03ed47b72ad9ee8b08bd4632e5b4154606'
-#     session['github_token'] = token
-    # user = User.query.filter_by(access_token=token).first()
-    # print user.access_token
 
 
 @app.route('/')
@@ -44,11 +32,36 @@ def create_user(token):
     return 'ok'
 
 
+def _get_repos(data):
+    _repos = {}
+    data = json.loads(data.raw_data)
+    for x in data:
+        if x['language'] and x['language'].lower() == 'python':
+            key = x['owner']['login']
+            l = [{
+                'name': x['name'],
+                'url': x['html_url'],
+            }]
+            if key in _repos:
+                _repos[key].append(l[0])
+            else:
+                _repos[key] = l
+    return _repos
+
+
 @app.route('/sync')
 def sync():
     if 'github_token' in session:
-        s = github.get('user/repos')
-        return s.raw_data
+        user_repos = github.get('user/repos')
+        user_repos = _get_repos(user_repos)
+
+        orgs = github.get('user/orgs')
+        for x in json.loads(orgs.raw_data):
+            org_repos = github.get('orgs/{0}/repos'.format(x['login']))
+            org_repos = _get_repos(org_repos)
+            user_repos = dict(user_repos, **org_repos)
+
+        return jsonify(user_repos)
     return redirect(url_for('login'))
 
 
